70b13093ea7be928c95488a117562e8bc3ec251f
zhuyanrui <2369702413@qq.com>
2019-01-05 10:22:58 +0800

    react

---
+++ src/containres/app.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../common/style.css'
+class App extends React.Component{
+    render(){
+        return <div></div>
+    }
+}
+export default App;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from 'view/app'
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
